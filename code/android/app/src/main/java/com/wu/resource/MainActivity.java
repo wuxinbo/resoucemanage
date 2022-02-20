981d9dec54d174a14bcdd1b62b9bf483677e1b08
wuxinbo <wuxinbo@cqcztech.com>
2022-02-20 22:12:28 +0800

    增加微信sdk。

--- code/android/app/src/main/java/com/wu/resource/MainActivity.java
+++ code/android/app/src/main/java/com/wu/resource/MainActivity.java
@@ -3,9 +3,11 @@ package com.wu.resource;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.PopupWindow;
 
 import com.bumptech.glide.Glide;
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView ;
     private GridView gridView;
+
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView =findViewById(R.id.imageView);
         gridView =findViewById(R.id.imageGridView);
-        gridView.addView(imageView);
+//        gridView.addView(imageView);
+        button =findViewById(R.id.share);
+        button.setOnClickListener((v)->{
+            PopupWindow popupWindow =new PopupWindow(MainActivity.this);
 
 
+        });
         Glide.with(this).load("http://192.168.1.112:8080/photo/get?mid=2570").into(imageView);
     }
+
+
 }
