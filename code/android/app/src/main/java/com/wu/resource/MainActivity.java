f3a8abf3050f1a7952a7bf81ab5fd03b3fb58b50
wuxinbo <wuxinbo@cqcztech.com>
2022-02-22 22:09:31 +0800

    popwindow 点击外部，关闭popwindow

--- code/android/app/src/main/java/com/wu/resource/MainActivity.java
+++ code/android/app/src/main/java/com/wu/resource/MainActivity.java
@@ -2,9 +2,11 @@ package com.wu.resource;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+//        getActionBar().hide();
 //        setActionBar();
         setContentView(R.layout.activity_main);
         imageView =findViewById(R.id.imageView);
@@ -32,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         popupWindow.setContentView(LayoutInflater.from(this).inflate(R.layout.share_image_view,null));
 //        gridView.addView(imageView);
         button =findViewById(R.id.share);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
         button.setOnClickListener((v)->{
             popupWindow.showAtLocation(gridView, Gravity.BOTTOM,0,0);
 
