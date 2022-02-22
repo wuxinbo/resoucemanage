7bd5a747f40d61bcba20bf9f976a730e6adbb620
xbwuc <wuxincai@outlook.com>
2022-02-22 23:21:07 +0800

    使用grid布局

--- code/android/app/src/main/java/com/wu/resource/MainActivity.java
+++ code/android/app/src/main/java/com/wu/resource/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wu.resource;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import android.widget.ListAdapter;
 import android.widget.PopupWindow;
 
 import com.bumptech.glide.Glide;
+import com.wu.resource.main.ShareListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,21 +28,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-//        getActionBar().hide();
-//        setActionBar();
         setContentView(R.layout.activity_main);
         imageView =findViewById(R.id.imageView);
         gridView =findViewById(R.id.imageGridView);
-
         popupWindow =new PopupWindow(MainActivity.this);
-        popupWindow.setContentView(LayoutInflater.from(this).inflate(R.layout.share_image_view,null));
+        popupWindow.setContentView(LayoutInflater.from(this).inflate(R.layout.share_layout,null));
 //        gridView.addView(imageView);
         button =findViewById(R.id.share);
+        RecyclerView recyclerView = (RecyclerView) popupWindow.getContentView().findViewById(R.id.shareLayout);
+        recyclerView.setAdapter(new ShareListAdapter());
+        recyclerView.setLayoutManager(new GridLayoutManager(this,6));
         popupWindow.setOutsideTouchable(true);
-        popupWindow.setFocusable(true);
         popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
         button.setOnClickListener((v)->{
             popupWindow.showAtLocation(gridView, Gravity.BOTTOM,0,0);
 
--- code/android/app/src/main/java/com/wu/resource/main/ShareListAdapter.java
+++ code/android/app/src/main/java/com/wu/resource/main/ShareListAdapter.java
@@ -1,68 +1,50 @@
 package com.wu.resource.main;
 
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 
-public class ShareListAdapter extends ListAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wu.resource.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShareListAdapter extends RecyclerView.Adapter<ShareListAdapter.ViewHolder> {
+    private static final List<Integer> items=new ArrayList<>();
+    static {
+        items.add(R.drawable.icon64_appwx_logo);
+        items.add(R.drawable.icon_res_download_moments);
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView ;
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            imageView =itemView.findViewById(R.id.imageView2);
+        }
+    }
+    @NonNull
     @Override
-    public boolean areAllItemsEnabled() {
-        return false;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.share_image_view, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public boolean isEnabled(int i) {
-        return false;
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView .setImageResource(items.get(position));
     }
 
     @Override
-    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
-
-    @Override
-    public int getItemViewType(int i) {
-        return 0;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public int getItemCount() {
+        return items.size();
     }
 }
