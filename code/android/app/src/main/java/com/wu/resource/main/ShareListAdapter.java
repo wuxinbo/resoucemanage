d9cc0d4a3d52e9250b4eee4c80d46e3276c071c4
xbwuc <wuxincai@outlook.com>
2022-02-26 11:11:41 +0800

    添加文字说明

--- code/android/app/src/main/java/com/wu/resource/main/ShareListAdapter.java
+++ code/android/app/src/main/java/com/wu/resource/main/ShareListAdapter.java
@@ -1,34 +1,78 @@
 package com.wu.resource.main;
 
+import static com.wu.resource.main.ShareListAdapter.shareInfo.Type.WX;
+
 import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
 import com.wu.resource.R;
+import com.wu.resource.WxShare;
+import com.wu.resource.databinding.ShareImageViewBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShareListAdapter extends RecyclerView.Adapter<ShareListAdapter.ViewHolder> {
-    private static final List<Integer> items=new ArrayList<>();
+    private static final List<shareInfo> items=new ArrayList<>();
     static {
-        items.add(R.drawable.icon64_appwx_logo);
-        items.add(R.drawable.icon_res_download_moments);
-        items.add(R.drawable.icon_res_download_collect);
+        items.add(new shareInfo(R.drawable.icon64_appwx_logo,R.string.share_wx_freind, WX.value));
+        items.add(new shareInfo(R.drawable.icon_res_download_moments,R.string.share_wx_freind, WX.value));
+        items.add(new shareInfo(R.drawable.icon_res_download_collect,R.string.share_wx_collect, WX.value));
+    }
+
+    static class shareInfo{
+        int resourceId;
+        int  labelId ;
+        int type;
+
+        public enum Type{
+            /**
+             * 微信
+             */
+            WX(1),
+            /**
+             * 抖音
+             */
+            DOUYING(2),
+            ;
+            int value ;
+
+            Type(int value) {
+                this.value = value;
+            }
+        }
+
+        public shareInfo(int resourceId, int label, int type) {
+            this.resourceId = resourceId;
+            this.labelId = label;
+            this.type = type;
+        }
     }
     class ViewHolder extends RecyclerView.ViewHolder{
         private ImageView imageView ;
-
+        private TextView textView ;
         public ViewHolder(@NonNull View itemView) {
 
             super(itemView);
-            imageView =itemView.findViewById(R.id.imageView2);
+            ShareImageViewBinding bind = ShareImageViewBinding.bind(itemView);
+            imageView = bind.imageView2;
+            textView = bind.lable;
+            imageView.setOnClickListener((view )->{
+                Toast.makeText(imageView.getContext(), "hello",Toast.LENGTH_SHORT).show();
+
+            });
             imageView.setBackgroundResource(R.drawable.share_item_selector);
         }
     }
@@ -42,7 +86,8 @@ public class ShareListAdapter extends RecyclerView.Adapter<ShareListAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.imageView .setImageResource(items.get(position));
+        holder.imageView .setImageResource(items.get(position).resourceId);
+        holder.textView.setText(items.get(position).labelId);
     }
 
     @Override
