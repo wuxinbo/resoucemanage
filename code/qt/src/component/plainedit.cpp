929dbbf53aad3e1ea5e15800a0c8bb4d4bd10d2f
xbwuc <wuxincai@outlook.com>
2022-05-29 15:33:09 +0800

    支持焦点判断

--- code/qt/src/component/plainedit.cpp
+++ code/qt/src/component/plainedit.cpp
@@ -1,27 +1,54 @@
-#include "plainedit.h"
+﻿#include "plainedit.h"
 #include <QPainter>
+#include <QFocusEvent>
 PlainEdit::PlainEdit(QWidget *parent)
     : QWidget{parent}
 {
     resize(100,100);
     setMinimumSize(MIN_WIDTH,MIN_HEIGHT);
     setFocusPolicy(Qt::ClickFocus);
+    textColor = new QColor(127, 127, 191);
+
+}
+
+PlainEdit::~PlainEdit()
+{
+    delete textColor;
+    delete backgrouodColor;
+    delete borderColor;
 }
 
 void PlainEdit::paintEvent(QPaintEvent *event){
     QPainter painter(this);
     qDebug("paint");
-    QColor textColor(0, 127, 127, 191);
-    QColor backGroundColor(0,0,0);
-    painter.setPen(textColor);
-    painter.setFont(QFont("华文宋体",12));
-    painter.drawText(10,10,"你好啊");
+    painter.setRenderHint(QPainter::RenderHint::Antialiasing);
+    QColor backGroundColor(255,255,255);
+    painter.setPen(*textColor);
+    QFont font("华文宋体",fontSize);
+    QFontMetrics metrics(font);
+    painter.setFont(font);
+    painter.setBrush(backGroundColor);
     painter.drawRoundedRect(QRectF(0,0,MIN_WIDTH,MIN_HEIGHT),RADIUS,RADIUS);
-    painter.setBackground(backGroundColor);
-
-
+    painter.drawText(10,metrics.height(),QStringLiteral("你好啊"));
+    if(focus){ //绘制焦点效果
+       painter.setBrush(Qt::BrushStyle::NoBrush);
+       painter.drawRoundedRect(QRectF(0,0,MIN_WIDTH,MIN_HEIGHT),RADIUS,RADIUS);
+    }
 }
 
 void PlainEdit::mousePressEvent(QMouseEvent *event){
-    qDebug("mousePress");
+}
+
+void PlainEdit::focusInEvent(QFocusEvent *event)
+{
+    focus = true;
+    update();
+    qDebug("getFcous");
+}
+
+void PlainEdit::focusOutEvent(QFocusEvent *event)
+{
+   focus=false;
+   update();
+   qDebug("lostFcous");
 }
--- code/qt/src/component/plainedit.h
+++ code/qt/src/component/plainedit.h
@@ -1,7 +1,8 @@
-#ifndef PLAINEDIT_H
+﻿#ifndef PLAINEDIT_H
 #define PLAINEDIT_H
 
 #include <QWidget>
+#include <QColor>
 
 class PlainEdit : public QWidget
 {
@@ -10,13 +11,31 @@ class PlainEdit : public QWidget
 
 public:
     explicit PlainEdit(QWidget *parent = nullptr);
+    ~ PlainEdit();
 
 protected:
+    /**
+     * @brief 行高由字体计算得来
+     */
+    int lineHeight;
+    /**
+     * @brief 默认为12
+     */
+    int fontSize =12;
     const int RADIUS =5;
     const int MIN_WIDTH=200;
     const int MIN_HEIGHT=50;
+
     void paintEvent(QPaintEvent *event) ;
     void mousePressEvent(QMouseEvent *event);
+    void focusInEvent(QFocusEvent *event) override;
+    void focusOutEvent(QFocusEvent *event) override;
+private:
+    QColor *textColor;
+    QColor *backgrouodColor;
+    QColor *borderColor;
+
+    bool  focus;
 signals:
 
 };
