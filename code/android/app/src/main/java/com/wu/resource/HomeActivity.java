5fbc6903d177c399e29ad168c77c639c0cacd0d1
xbwuc <wuxincai@outlook.com>
2024-03-18 23:21:53 +0800

    1.底部导航栏透明。 2.状态栏透明。 3.按照拍摄时间分组。

--- code/android/app/src/main/java/com/wu/resource/HomeActivity.java
+++ code/android/app/src/main/java/com/wu/resource/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.wu.resource;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,18 +16,19 @@ import com.wu.resource.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ActivityHomeBinding binding;
+  private ActivityHomeBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        NavController navController = Navigation.findNavController(this, R.id.nav_bottom_home);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+    binding = ActivityHomeBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    //系统栏后布置您的应用
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    View decorView = getWindow().getDecorView();
+    NavController navController = Navigation.findNavController(this, R.id.nav_bottom_home);
+    NavigationUI.setupWithNavController(binding.navView, navController);
+  }
 
 }
--- code/android/app/src/main/java/com/wu/resource/ui/home/HomeFragment.java
+++ code/android/app/src/main/java/com/wu/resource/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.wu.resource.ui.home;
 
+import static com.wu.resource.Constant.DATE_FORMAT;
 import static com.wu.resource.Constant.gson;
 
 import android.os.Build;
@@ -11,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -21,70 +21,76 @@ import com.wu.common.http.HttpUtil;
 import com.wu.resource.Constant;
 import com.wu.resource.R;
 import com.wu.resource.ResourceApplication;
-import com.wu.resource.databinding.FragmentHomeBinding;
+import com.wu.resource.databinding.FragmentPicBinding;
 import com.wu.resource.image.PhotoInfo;
 import com.wu.resource.image.PhotoListAdapter;
 import com.wu.resource.image.PhotoResponse;
 import com.wu.sphinxsearch.NativeLib;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-    private RecyclerView gridView;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-//        HomeViewModel homeViewModel =
-//                new ViewModelProvider(this).get(HomeViewModel.class);
+  private FragmentPicBinding binding;
+  private RecyclerView gridView;
+  private SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container, Bundle savedInstanceState) {
+    binding = FragmentPicBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+    NativeLib nativeLib = new NativeLib();
+    Log.i("native test", nativeLib.query("50mm", "photo"));
+    gridView = binding.photoGrid;
 
-        NativeLib nativeLib =new NativeLib();
-        Log.i("native test",nativeLib.query("50mm","photo"));
-        //setContentView(R.layout.activity_main);
-        gridView = root.findViewById(R.id.photoGrid);
-        //初始化相册数据
-        ResourceApplication application = (ResourceApplication) getActivity().getApplication();
-        HttpUtil.executorService.execute(()->{
-            Handler handler = null;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                handler = Handler.createAsync(Looper.getMainLooper());
-            }
-            List<PhotoInfo> list = application.getDb().photoDao().getAll();
-            handler.post(() -> {
-                gridView.setAdapter(new PhotoListAdapter(list, getContext()));
-                gridView.setLayoutManager(new GridLayoutManager(getContext(), 4));
-
-            });
-        });
-        //查询数据
-        HttpUtil.executorService.execute(() -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                HttpUtil.getJson(Constant.URL + "photo/listByPage", (result) -> {
-                    PhotoResponse photoResponse = gson.fromJson(result, PhotoResponse.class);
-                    /**
-                     *
-                     */
-                    List<PhotoInfo> content = photoResponse.getContent();
-                    Handler handler = Handler.createAsync(Looper.getMainLooper());
-                    application.getDb().photoDao().insertAll(content);
-                    handler.post(() -> {
-                        gridView.setAdapter(new PhotoListAdapter(content, getContext()));
-                        gridView.setLayoutManager(new GridLayoutManager(getContext(), 4));
-
-                    });
-                });
-            }
+    //初始化相册数据
+    ResourceApplication application = (ResourceApplication) getActivity().getApplication();
+    HttpUtil.executorService.execute(() -> {
+      Handler handler = null;
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        handler = Handler.createAsync(Looper.getMainLooper());
+      }
+      List<PhotoInfo> list = application.getDb().photoDao().getAll();
+      handler.post(() -> {
+        Map<String, List<PhotoInfo>> collect = list.stream().collect(Collectors.groupingBy(it -> dateFormat.format(it.getShotTime())));
+        collect.forEach((key,value)->{
+          binding.date.setText(key);
+          gridView.setAdapter(new PhotoListAdapter(value, getContext()));
+          gridView.setLayoutManager(new GridLayoutManager(getContext(), 4));
         });
 
-        return root;
-    }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+      });
+    });
+    //查询数据
+    HttpUtil.executorService.execute(() -> {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        HttpUtil.getJson(Constant.URL + "photo/listByPage", (result) -> {
+          PhotoResponse photoResponse = gson.fromJson(result, PhotoResponse.class);
+          /**
+           *
+           */
+          List<PhotoInfo> content = photoResponse.getContent();
+          Handler handler = Handler.createAsync(Looper.getMainLooper());
+          application.getDb().photoDao().insertAll(content);
+          handler.post(() -> {
+            gridView.setAdapter(new PhotoListAdapter(content, getContext()));
+            gridView.setLayoutManager(new GridLayoutManager(getContext(), 4));
+
+          });
+        });
+      }
+    });
+
+    return root;
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
 }
