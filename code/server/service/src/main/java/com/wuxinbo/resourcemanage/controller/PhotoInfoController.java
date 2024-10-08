a30ceb6aded256f549c21c3d427c289557ec0fb1
xbwuc <wuxincai@outlook.com>
2024-10-08 23:10:39 +0800

    1.更新是否喜欢图片。

--- code/server/service/src/main/java/com/wuxinbo/resourcemanage/controller/PhotoInfoController.java
+++ code/server/service/src/main/java/com/wuxinbo/resourcemanage/controller/PhotoInfoController.java
@@ -1,15 +1,13 @@
 package com.wuxinbo.resourcemanage.controller;
 
-import com.wuxinbo.resourcemanage.model.BarChartData;
-import com.wuxinbo.resourcemanage.model.Constant;
-import com.wuxinbo.resourcemanage.model.PhotoGroupBy;
-import com.wuxinbo.resourcemanage.model.PhotoInfo;
+import com.wuxinbo.resourcemanage.model.*;
 import com.wuxinbo.resourcemanage.reposity.PhotoInfoReposity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,107 +25,124 @@ import java.util.*;
 @RequestMapping("photo")
 @CrossOrigin("*")
 public class PhotoInfoController extends BaseController {
-  @Autowired
-  PhotoInfoReposity photoInfoReposity;
+    @Autowired
+    PhotoInfoReposity photoInfoReposity;
 
-  @RequestMapping("get")
-  void getPhoto(Integer mid, HttpServletRequest request, HttpServletResponse response) {
-    Optional<PhotoInfo> photo = photoInfoReposity.findById(mid);
-    photo.ifPresent(it -> {
-      try {
-        try (
-          FileInputStream fis = new FileInputStream(it.getSysFileStoreItem().getSysFileStoreNode().getLocalPath() + it.getSysFileStoreItem().getRelativeUrl())
-        ) {
-          ServletOutputStream outputStream = response.getOutputStream();
-          byte[] data = new byte[2048];
-          int length = 0;
-          while ((length = fis.read(data)) != -1) {
-            outputStream.write(data, 0, length);
-          }
-          outputStream.close();
+    @RequestMapping("get")
+    void getPhoto(Integer mid, HttpServletRequest request, HttpServletResponse response) {
+        Optional<PhotoInfo> photo = photoInfoReposity.findById(mid);
+        photo.ifPresent(it -> {
+            try {
+                try (
+                        FileInputStream fis = new FileInputStream(it.getSysFileStoreItem().getSysFileStoreNode().getLocalPath() + it.getSysFileStoreItem().getRelativeUrl())
+                ) {
+                    ServletOutputStream outputStream = response.getOutputStream();
+                    byte[] data = new byte[2048];
+                    int length = 0;
+                    while ((length = fis.read(data)) != -1) {
+                        outputStream.write(data, 0, length);
+                    }
+                    outputStream.close();
+                }
+            } catch (FileNotFoundException e) {
+                logger.error("FileNotFoundException", e);
+            } catch (IOException e) {
+                logger.error("FileNotFoundException", e);
+            }
+        });
+    }
+
+    @RequestMapping("listByPage")
+    Page<PhotoInfo> listByPage(Integer pageSize, Integer currentPage,
+                               HttpServletRequest request,
+                               HttpServletResponse response) {
+
+        Pageable page = PageRequest.of(currentPage == null ? 0 : currentPage,
+                pageSize == null ? Constant.DEFAULT_PAGESIZE : pageSize);
+
+        Page<PhotoInfo> all = photoInfoReposity.findBySysFileStoreItemFileType(page, "jpg");
+        return all;
+    }
+
+    /**
+     * 更新
+     * @param photoInfoList
+     * @return
+     */
+    @RequestMapping("/update")
+    protected Result update(@RequestBody List<PhotoInfo> photoInfoList) {
+        if (photoInfoList != null && !photoInfoList.isEmpty()) {
+            for (PhotoInfo photoInfo : photoInfoList) {
+                photoInfoReposity.updateLike(photoInfo.getLike(),photoInfo.getMid());
+            }
         }
-      } catch (FileNotFoundException e) {
-        logger.error("FileNotFoundException", e);
-      } catch (IOException e) {
-        logger.error("FileNotFoundException", e);
-      }
-    });
-  }
-
-  @RequestMapping("listByPage")
-  Page<PhotoInfo> listByPage(Integer pageSize, Integer currentPage,
-                             HttpServletRequest request,
-                             HttpServletResponse response) {
-
-    Pageable page = PageRequest.of(currentPage == null ? 0 : currentPage,
-      pageSize == null ? Constant.DEFAULT_PAGESIZE : pageSize);
-
-    Page<PhotoInfo> all = photoInfoReposity.findBySysFileStoreItemFileType(page, "jpg");
-    return all;
-  }
-
-  /**
-   * 根据拍摄日期查询照片
-   * @param shotDate
-   * @return
-   */
-  @RequestMapping("listByShotDate")
-  List<PhotoInfo> listByShotDate(String shotDate){
-    if (shotDate==null ||shotDate.equals("")){
-      return new ArrayList<>();
+        return Result.success();
     }
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constant.DATE_FORMAT);
-    try {
-      Date date = simpleDateFormat.parse(shotDate);
-      Calendar instance = Calendar.getInstance();
-      instance.setTime(date);
-      instance.add(Calendar.DATE,1);
-      return photoInfoReposity.findByshotTimeBetween(date,instance.getTime());
-    } catch (ParseException e) {
-      logger.error("parseException",e);
-    }
-    return new ArrayList<>();
-  }
-  /**
-   * 分组查询
-   *
-   * @param group
-   * @param request
-   * @param response
-   * @return
-   */
-  @RequestMapping("queryPhotoGroupby")
-  BarChartData queryPhotoGroupby(Integer group,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response) {
-    List list = null;
-    PhotoGroupBy groupEnum
-      = PhotoGroupBy.getEnumByValue(group.intValue());
-    if (groupEnum != null) {
-      switch (groupEnum) {
-        case LENS:
-          list = photoInfoReposity.queryPhotoGroupByLens();
-          break;
-        case FOUCUS_LENGTH:
-          list = photoInfoReposity.queryPhotoGroupByFoucus();
-          break;
-        case SHOT_TIME:
-          list = photoInfoReposity.queryPhotoGroupByShotTime();
-        case ALL_SHOT_TIME:
-          list = photoInfoReposity.queryPhotoGroupByShotTimeAll();
-      }
-    }
-    BarChartData data = new BarChartData();
-    if (list != null && !list.isEmpty()) {
-      for (Object item : list) {
-        Object[] o = (Object[]) item;
-        data.addCategory(o[0].toString());
-        data.addData(o[1]);
-      }
-    }
-    return data;
 
-  }
+    /**
+     * 根据拍摄日期查询照片
+     *
+     * @param shotDate
+     * @return
+     */
+    @RequestMapping("listByShotDate")
+    List<PhotoInfo> listByShotDate(String shotDate) {
+        if (shotDate == null || shotDate.equals("")) {
+            return new ArrayList<>();
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constant.DATE_FORMAT);
+        try {
+            Date date = simpleDateFormat.parse(shotDate);
+            Calendar instance = Calendar.getInstance();
+            instance.setTime(date);
+            instance.add(Calendar.DATE, 1);
+            return photoInfoReposity.findByshotTimeBetween(date, instance.getTime());
+        } catch (ParseException e) {
+            logger.error("parseException", e);
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     * 分组查询
+     *
+     * @param group
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("queryPhotoGroupby")
+    BarChartData queryPhotoGroupby(Integer group,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response) {
+        List list = null;
+        PhotoGroupBy groupEnum
+                = PhotoGroupBy.getEnumByValue(group.intValue());
+        if (groupEnum != null) {
+            switch (groupEnum) {
+                case LENS:
+                    list = photoInfoReposity.queryPhotoGroupByLens();
+                    break;
+                case FOUCUS_LENGTH:
+                    list = photoInfoReposity.queryPhotoGroupByFoucus();
+                    break;
+                case SHOT_TIME:
+                    list = photoInfoReposity.queryPhotoGroupByShotTime();
+                case ALL_SHOT_TIME:
+                    list = photoInfoReposity.queryPhotoGroupByShotTimeAll();
+            }
+        }
+        BarChartData data = new BarChartData();
+        if (list != null && !list.isEmpty()) {
+            for (Object item : list) {
+                Object[] o = (Object[]) item;
+                data.addCategory(o[0].toString());
+                data.addData(o[1]);
+            }
+        }
+        return data;
+
+    }
 
 
 }
--- code/server/service/src/main/java/com/wuxinbo/resourcemanage/reposity/PhotoInfoReposity.java
+++ code/server/service/src/main/java/com/wuxinbo/resourcemanage/reposity/PhotoInfoReposity.java
@@ -3,46 +3,65 @@ package com.wuxinbo.resourcemanage.reposity;
 import com.wuxinbo.resourcemanage.model.PhotoInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
 
 @Repository
-public interface PhotoInfoReposity extends PagingAndSortingRepository<PhotoInfo,Integer> {
+public interface PhotoInfoReposity extends PagingAndSortingRepository<PhotoInfo, Integer> {
     /**
      * 通过Id去重
+     *
      * @param fileId
      * @return
      */
     List<PhotoInfo> findByFileId(Integer fileId);
-    @Query(value = "select i.*,item.relative_url from photo_info i left join sys_file_store_item item on i.file_id =item.mid " +
-      "where item.mid is not null " +
-      "and item.file_type ='jpg'" +
-      " and relative_url like '%export%'" +
-      "order by i.shot_time desc ",nativeQuery = true,
-      countQuery = "select count(1) from photo_info i left join sys_file_store_item item on" +
-        " i.file_id =item.mid where item.mid is not null and item.file_type ='jpg' and relative_url like '%export%'"
-    )
-    Page<PhotoInfo> findBySysFileStoreItemFileType(Pageable page,String fileType);
 
-  /**
-   * 通过拍摄日期查询
-   * @return
-   */
-  @Query(value = "select i.*,item.relative_url from photo_info i left join sys_file_store_item item on i.file_id =item.mid " +
-    "where item.mid is not null " +
-    "and item.file_type ='jpg'" +
-    " and item.relative_url like '%export%'" +
-    "and i.shot_time between :startDate and :endDate "+
-    "order by i.shot_time desc ",nativeQuery = true
-  )
-  List<PhotoInfo> findByshotTimeBetween(Date startDate, Date endDate);
+    @Query(value = "select i.*,item.relative_url from photo_info i left join sys_file_store_item item on i.file_id =item.mid " +
+            "where item.mid is not null " +
+            "and item.file_type ='jpg'" +
+            " and relative_url like '%export%'" +
+            "order by i.shot_time desc ", nativeQuery = true,
+            countQuery = "select count(1) from photo_info i left join sys_file_store_item item on" +
+                    " i.file_id =item.mid where item.mid is not null and item.file_type ='jpg' and relative_url like '%export%'"
+    )
+    Page<PhotoInfo> findBySysFileStoreItemFileType(Pageable page, String fileType);
+
+
+    /**
+     * 更新like
+     *
+     * @param it
+     */
+    @Transactional
+    @Modifying
+    @Query(value = "update photo_info set `like` =:like,update_time=now() where mid =:mid", nativeQuery = true)
+    void updateLike(Integer like,Integer mid);
+
+    /**
+     * 通过拍摄日期查询
+     *
+     * @return
+     */
+    @Query(value = "select i.*,item.relative_url from photo_info i left join sys_file_store_item item on i.file_id =item.mid " +
+            "where item.mid is not null " +
+            "and item.file_type ='jpg'" +
+            " and item.relative_url like '%export%'" +
+            "and i.shot_time between :startDate and :endDate " +
+            "order by i.shot_time desc ", nativeQuery = true
+    )
+    List<PhotoInfo> findByshotTimeBetween(Date startDate, Date endDate);
+
     /**
      * 根据镜头统计
+     *
      * @return
      */
     @Query("select lens,count(1) from PhotoInfo where lens is not null group by lens order by count(1)")
@@ -50,18 +69,21 @@ public interface PhotoInfoReposity extends PagingAndSortingRepository<PhotoInfo,
 
     /**
      * 根据焦段统计
+     *
      * @return
      */
     @Query("select focusLength,count(1) from PhotoInfo where focusLength is not null group by focusLength order by count(1)")
     List queryPhotoGroupByFoucus();
+
     @Query(value = "select date_format(i.shot_time,'%Y-%m-%d'),count(1) from photo_info i left join sys_file_store_item s on i.file_id=s.mid" +
             " where i.shot_time is not null and s.relative_url like '%export%'" +
             "group by date_format(i.shot_time,'%Y-%m-%d')\n" +
-            " order by count(1) desc limit 0,20",nativeQuery = true)
+            " order by count(1) desc limit 0,20", nativeQuery = true)
     List queryPhotoGroupByShotTime();
-  @Query(value = "select date_format(i.shot_time,'%Y-%m-%d'),count(1) from photo_info i left join sys_file_store_item s on i.file_id=s.mid" +
-    " where i.shot_time is not null and s.relative_url like '%export%'" +
-    "group by date_format(i.shot_time,'%Y-%m-%d')\n" +
-    " order by date_format(i.shot_time,'%Y-%m-%d') desc ",nativeQuery = true)
-  List queryPhotoGroupByShotTimeAll();
+
+    @Query(value = "select date_format(i.shot_time,'%Y-%m-%d'),count(1) from photo_info i left join sys_file_store_item s on i.file_id=s.mid" +
+            " where i.shot_time is not null and s.relative_url like '%export%'" +
+            "group by date_format(i.shot_time,'%Y-%m-%d')\n" +
+            " order by date_format(i.shot_time,'%Y-%m-%d') desc ", nativeQuery = true)
+    List queryPhotoGroupByShotTimeAll();
 }
