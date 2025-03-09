8612b7f40ed581b7ad618979f8502e9b8ce74c6f
wuxinbo <wuxincai@outlook.com>
2025-03-09 23:16:42 +0800

    增加文件压缩jni接口

---
+++ code/server/service/src/main/java/com/wuxinbo/resourcemanage/jni/ImageMagick.java
@@ -0,0 +1,35 @@
+package com.wuxinbo.resourcemanage.jni;
+
+/**
+ * imageMagick 图片处理 jni 接口
+ */
+public class ImageMagick {
+
+
+    static {
+        System.loadLibrary("core");
+
+        init();
+    }
+
+    /**
+     * 系统启动时初始化
+     */
+    public static native void init();
+
+    /**
+     * 图片缩放 
+     * @param src 原始图片地址
+     * @param dest 目标图片地址
+     * @param scale 缩放比例，计算公式为 新分辨率 = 原始分辨率/scale，
+     * 比如原始图片分辨率为1920*1080，scale=10，则新分辨率为192*108
+     * @return code 0:成功，-1:失败
+     */
+    public static native int resize(String src, String dest, int scale);
+
+    /**
+     * 资源销毁
+     */
+    public static native void destroy();
+    
+}
