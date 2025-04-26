82847b350b468e9ddc2fb0cfc9968cbb7e982447
xbwuc <wuxincai@outlook.com>
2025-04-26 23:16:18 +0800

    日志文件输出增加行号

--- code/native/base/logger.cpp
+++ code/native/base/logger.cpp
@@ -2,43 +2,46 @@
 // Created by wuxinbo on 2025/4/19.
 //
 #include "logger.h"
-#ifdef  ANDROID
+#include "Poco/FileChannel.h"
+#ifdef ANDROID
 #include <android/log.h>
 #endif
+#include "Poco/ConsoleChannel.h"
 #include "Poco/Format.h"
-#include "Poco/Logger.h"
-#include "Poco/ConsoleChannel.h"
 #include "Poco/FormattingChannel.h"
+#include "Poco/Logger.h"
 #include "Poco/PatternFormatter.h"
-#include "Poco/NumberFormatter.h"
-#include "Poco/ConsoleChannel.h"
+#include <iostream>
 
+using Poco::ConsoleChannel;
 using Poco::FormattingChannel;
 using Poco::PatternFormatter;
-using Poco::ConsoleChannel;
 
 FormattingChannel *pFCConsole;
 
-xbwuc::Logger::Logger() {
+xbwuc::Logger::Logger() {}
+
+void xbwuc::Logger::info(const char *fileName, int linenumber, const char *tag,
+                         std::string fmt, std::string data...) {
 #ifndef ANDROID
-    if(pFCConsole) return;
-    pFCConsole = new FormattingChannel( new PatternFormatter("[%O] %s: %p: %t"));
+  if (!pFCConsole) {
+    pFCConsole = new FormattingChannel(
+        new PatternFormatter("[%O(%u)] %Y-%m-%d %H:%M:%S.%i  %p: %t"));
+     
     pFCConsole->setChannel(new ConsoleChannel);
     pFCConsole->open();
     Poco::Logger::create("ConsoleLogger", pFCConsole);
+  }
+
 #endif
-}
-
-void xbwuc::Logger::info(const char *tag, std::string fmt, std::string data...) {
-
-#ifdef  ANDROID
-    __android_log_print(android_LogPriority::ANDROID_LOG_INFO, tag, Poco::format(fmt, data).c_str(),
-                        data[0]);
-    return;
+#ifdef ANDROID
+  __android_log_print(android_LogPriority::ANDROID_LOG_INFO, tag,
+                      Poco::format(fmt, data).c_str(), data[0]);
+  return;
 #endif
 
-    Poco::Logger &consoleLogger = Poco::Logger::get("ConsoleLogger");
-    consoleLogger.information(Poco::format(fmt, data).c_str(), "");
-
+  Poco::Logger &consoleLogger = Poco::Logger::get("ConsoleLogger");
+  consoleLogger.information(
+      Poco::format(Poco::format("[%s]  %s", std::string(tag), fmt), data),
+      fileName, linenumber);
 }
-
--- code/native/base/logger.h
+++ code/native/base/logger.h
@@ -1,21 +1,29 @@
 //
 // 日志输出封装
 //
-#include "common.h"
+
 #ifndef RESOURCE_LOGGER_H
 #define RESOURCE_LOGGER_H
+
+
+#define LOG_INFO_DATA2(tag,fmt,data1,data2)  xbwuc::Logger::info(__FILE__,__LINE__,tag, fmt, data1,data2);
+#define LOG_INFO_DATA(tag,fmt,data1)  xbwuc::Logger::info(__FILE__,__LINE__,tag, fmt, data1);
+#define LOG_INFO(tag,fmt)  xbwuc::Logger::info(__FILE__,__LINE__,tag, fmt, "");
+#include "common.h"
 #include <string>
 XBWUC_NAMESPACE_START
 class LoggerImpl;
 // class
 class Logger {
 private:
+
     LoggerImpl * loggerImpl;
 public:
+    // 初始化
     Logger();
     ~Logger();
     // 日志输出
-    static void info(const char * tag,std::string fmt,std::string data... );
+    static void info(const char *  fileName,int linenumber, const char * tag,std::string fmt,std::string data... );
 
 };
 
